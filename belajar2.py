22abf3a0225684b36af6784cfc63e035265d47e4
diancahya1 <dianc906@gmail.com>
2025-03-09 21:00:54 +0700

    Update belajar2.py

---
+++ belajar2.py
@@ -0,0 +1,46 @@
+#operasi komparasi 
+# setiap hasil dari operasi komparasi adalah boolean
+
+a=4
+b=2
+# lebih dari (>)
+print ("=========== lebih dari (>)")
+hasil= a > 3
+print (a,'>',3,hasil)
+hasil= 2 > 3
+print (2,'>',3,hasil)
+
+# kurang dari (<)
+print ("=========== kurang dari (<)")
+hasil= a < 3
+print (a,'<',3,hasil)
+hasil= 2 < 3
+print (2,'<',3,hasil)
+
+# lebih dari sama dengan (>=)
+print ("=========== lebih dari sama dengan (>=)")
+hasil= a >= 3
+print (a,'>=',3,hasil)
+hasil= 2 >= 3
+print (2,'>=',3,hasil)
+hasil= a >= 4
+print (a,'>=',4,hasil)
+
+# kurang dari sama dengan (<=)
+print ("=========== kurang dari sama dengan (<=)")
+hasil= a <= 3
+print (a,'<=',3,hasil)
+hasil= 2 <= 3
+print (2,'<=',3,hasil)
+hasil= a <= 4
+print (a,'<=',4,hasil)
+
+#  sama dengan (==) / adapun juga tidak sama dengan(!=)
+print ("===========  sama dengan (==)")
+hasil= a == 3
+print (a,'==',3,hasil)
+hasil= 2 == 3
+print (2,'==',3,hasil)
+hasil= a == 4
+print (a,'==',4,hasil)
+
